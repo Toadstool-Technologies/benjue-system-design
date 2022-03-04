04177ff8bac4c9bc52c28bb308300a2cc38ea109
Benjue Han <vivianhan0620@gmail.com>
2022-03-03 19:20:57 -0800

    extract and loaded

--- schema.sql
+++ schema.sql
@@ -1,31 +1,44 @@
 drop schema if exists qa;
 CREATE SCHEMA IF NOT EXISTS qa;
 
-drop table if exists products;
-drop table if exists questions;
-drop table if exists answers;
+drop table if exists products cascade;
+drop table if exists questions cascade;
+drop table if exists answers cascade;
+drop table if exists photos cascade;
 
 CREATE TABLE products (
-  id serial,
-  product_id int not null unique,
-  primary key(id)
+  id serial primary key,
+  name varchar(100) not null,
+  slogan varchar(200),
+  description varchar(500),
+  category varchar(30),
+  default_price int
 );
 
 CREATE TABLE questions (
   id serial primary key,
-  question_body varchar(400) not null,
-  question_date timestamp,
+  product_id int references products(id),
+  body varchar(400) not null,
+  date_written bigint,
   asker_name varchar(100) not null,
-  question_helpfulness smallint,
+  asker_email varchar(100),
   reported boolean,
-  product_id int references products(id)
+  helpful smallint
 );
 
 CREATE TABLE answers (
   id serial primary key,
+  question_id int references questions(id),
   body varchar(1000) not null,
-  answer_date timestamp,
+  date_written bigint,
   answerer_name varchar(100) not null,
-  answer_helpfulness smallint,
-  photos text []
+  answerer_email varchar(100),
+  reported boolean,
+  helpful smallint
 );
+
+CREATE TABLE photos (
+  id serial primary key,
+  answer_id int references answers(id),
+  url varchar(150)
+)
